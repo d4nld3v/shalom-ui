0c5543677058954106853e023c0bc88c871be6a9
enrbaewebdaniel <enrbaewebdaniel@gmail.com>
2025-03-12 18:10:14 +0100

    feat: add button component with customizable styles and click event

--- src/components/button/button.component.ts
+++ src/components/button/button.component.ts
@@ -2,18 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'storybook-button',
+  selector: 'sl-button',
   standalone: true,
   imports: [CommonModule],
-  template: ` <button
-  type="button"
-  (click)="onClick.emit($event)"
-  [ngClass]="classes"
-  [ngStyle]="{ 'background-color': backgroundColor }"
->
-  {{ label }}
-</button>`,
-  styleUrls: ['./button.css'],
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.scss'],
 })
 export class ButtonComponent {
   /** Is this the principal call to action on the page? */
@@ -41,8 +34,8 @@ export class ButtonComponent {
   onClick = new EventEmitter<Event>();
 
   public get classes(): string[] {
-    const mode = this.primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+    const mode = this.primary ? 'button--primary' : 'button--secondary';
 
-    return ['storybook-button', `storybook-button--${this.size}`, mode];
+    return ['button', `button--${this.size}`, mode];
   }
 }
